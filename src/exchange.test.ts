d0139945cc8b520549e4414426540975a329ab67
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2023-11-18 19:11:48 -0500

    fix

--- src/exchange.test.ts
+++ src/exchange.test.ts
@@ -1,15 +1,9 @@
-import { getExchange } from "./exchange";
-import { getTokens } from "./tokens";
+import { getTrade } from "./exchange";
 
-const USER_ADDRESS = "0x9540E559e170E8a817ce0F9f22969d592613Bc5a";
+const USER_ADDRESS = "0xeffbdc84661F37BA85a1F951E5424cab8e89D28b";
 
 test("Fetches price", async () => {
-    const tokens = await getTokens();
-
-    const linkToken = tokens.find(token => token.symbol === "LINK")!;
-    const grtToken = tokens.find(token => token.symbol === "GRT")!;
-
-    const price = await getExchange(USER_ADDRESS, linkToken, grtToken, 10);
+    const price = await getTrade(USER_ADDRESS, "LINK", "GRT", 10);
 
     expect(price.sellAmount).toBe("10.0");
     expect(Number.parseFloat(price.buyAmount)).toBeGreaterThan(100);
