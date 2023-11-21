c0b13bc367c6b8dca1d8d48125cd70a7c4543038
Adam Balogh <adambalogh@Adams-MacBook-Air.local>
2023-11-21 18:55:24 -0500

    prompt

--- src/bot.ts
+++ src/bot.ts
@@ -139,7 +139,9 @@ export class Bot {
 
                     return {
                         tool_call_id: call.id,
-                        output: `Execute trade by clicking on "${coinBaseWalletUrl(exchangeUrl)}"`
+                        output: `The user can swap ${trade.sellAmount} ${trade.sell.name} for ${trade.buyAmount} ${trade.buy.name} 
+                        by clicking on this link that will take the user to his/her wallet (to a trusted website)
+                        "${coinBaseWalletUrl(exchangeUrl)}"`
                     };
 
                 } else if (call.function.name === 'get_user_tokens') {
